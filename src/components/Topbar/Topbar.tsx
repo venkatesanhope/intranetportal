f0aacf47c8ac7d00c6cbce97c84f2e623e91a8a2
venkatesanhope <venkatesan.hope@gmail.com>
2023-07-15 17:29:56 +0530

    update

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -4,7 +4,7 @@ const Topbar =()=>{
     return(
         <>
             <nav  className="navbar navbar-expand navbar-light justify-content-center bg-white topbar mb-4 static-top shadow">
-        <div className="text-center my-auto"> <span  className ="fontUpdateStyle">QUICK SORT INTRANET PORTAL</span> </div>
+        <div className="text-center my-auto"> <span  className ="fontUpdateStyle"><h3>QUICK SORT INTRANET PORTAL</h3></span> </div>
       </nav>
         </>
     )
